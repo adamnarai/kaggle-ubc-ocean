8f1b159d251cae1418f09024aa99f23f5d6bed6f
latlab <latlab@gmail.com>
2023-11-02 13:54:23 +0100

    major restructure

--- src/dataset.py
+++ src/dataset.py
@@ -8,7 +8,7 @@ from torchvision import transforms
 class UBCDataset(Dataset):
     """UBC OCEAN dataset."""
 
-    def __init__(self, df, image_dir, thumbnail_dir, encode, transform=None):
+    def __init__(self, df, image_dir, thumbnail_dir, transform=None):
         """
         Arguments:
             df (DataFrame): DataFrame of annotations.
@@ -18,7 +18,6 @@ class UBCDataset(Dataset):
                 on a sample.
         """
         self.df = df
-        self.df['label'] = self.df['label'].map(encode)
         self.image_ids = self.df['image_id'].values
         self.labels = self.df['label'].values
         self.image_dir = image_dir
@@ -43,6 +42,41 @@ class UBCDataset(Dataset):
             image = self.transform(image)
 
         return image, label
+    
+class UBCTilesDataset(Dataset):
+    """UBC OCEAN tiles dataset."""
+
+    def __init__(self, df, image_dir, transform=None):
+        """
+        Arguments:
+            df (DataFrame): DataFrame of annotations.
+            image_dir (string): Directory with all the fullres images.
+            transform (callable, optional): Optional transform to be applied
+                on a sample.
+        """
+        self.df = df
+        self.image_ids = self.df['image_id'].values
+        self.orig_image_ids = self.df['orig_image_id'].values
+        self.labels = self.df['label'].values
+        self.is_tma = self.df['is_tma'].values
+        self.image_dir = image_dir
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.df)
+
+    def __getitem__(self, idx):
+        image_id = self.image_ids[idx]
+        orig_image_id = self.orig_image_ids[idx]
+        image_path = os.path.join(self.image_dir, orig_image_id, f'{image_id}.png')
+
+        image = Image.open(image_path)
+        label = self.labels[idx]
+        
+        if self.transform:
+            image = self.transform(image)
+
+        return image, label
 
 class SideCrop(nn.Module):
     def forward(self, image):
@@ -50,7 +84,7 @@ class SideCrop(nn.Module):
         new_image = transforms.functional.crop(image, 0, 0, size, size)
         return new_image
 
-def get_datasets(CFG, train_image_dir, train_thumbnail_dir, df_train, df_validation, encode):
+def get_datasets(CFG, train_image_dir, train_thumbnail_dir, df_train, df_validation):
     transform = {
     'train':
     transforms.Compose([
@@ -73,12 +107,39 @@ def get_datasets(CFG, train_image_dir, train_thumbnail_dir, df_train, df_validat
     train_dataset = UBCDataset(df=df_train, 
                         image_dir=train_image_dir, 
                         thumbnail_dir=train_thumbnail_dir,
-                        encode=encode,
                         transform=transform['train'])
     validation_dataset = UBCDataset(df=df_validation, 
                         image_dir=train_image_dir, 
                         thumbnail_dir=train_thumbnail_dir,
-                        encode=encode,
+                        transform=transform['validation'])
+    datasets = {'train': train_dataset, 'validation': validation_dataset}
+    return datasets
+
+def get_tiles_datasets(CFG, train_image_dir, df_train, df_validation):
+    transform = {
+    'train':
+    transforms.Compose([
+        SideCrop(),
+        transforms.Resize(CFG['img_size']),
+        transforms.RandomAffine(degrees=CFG['affine_degrees'], translate=CFG['affine_translate'], scale=CFG['affine_scale']),
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomVerticalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    ]),
+    'validation':
+     transforms.Compose([
+        SideCrop(),
+        transforms.Resize(CFG['img_size']),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    ])}
+    
+    train_dataset = UBCTilesDataset(df=df_train, 
+                        image_dir=train_image_dir,
+                        transform=transform['train'])
+    validation_dataset = UBCTilesDataset(df=df_validation, 
+                        image_dir=train_image_dir,
                         transform=transform['validation'])
     datasets = {'train': train_dataset, 'validation': validation_dataset}
     return datasets
--- src/trainer/trainer.py
+++ src/trainer/trainer.py
@@ -5,106 +5,87 @@ import warnings
 from sklearn.metrics import balanced_accuracy_score
 import numpy as np
 import wandb
+from tqdm import tqdm
 
 import torch
 
 class Trainer:
-    def __init__(self, model, dataloaders, criterion, optimizer, scheduler, device, num_epochs=25, wandb_log=False):
+    def __init__(self, model, dataloaders, loss_fn, optimizer, scheduler, device, metric, num_epochs=10, wandb_log=False):
         self.model = model
-        self.dataloaders = dataloaders
-        self.criterion = criterion
+        self.train_dataloader = dataloaders['train']
+        self.test_dataloader = dataloaders['validation']
+        self.loss_fn = loss_fn
         self.optimizer = optimizer
         self.scheduler = scheduler
         self.device = device
+        self.metric = metric
         self.num_epochs = num_epochs
         self.wandb_log = wandb_log
-        self.dataset_sizes = {x: len(self.dataloaders[x].dataset) for x in ['train', 'validation']}
-    
-    def set_num_epochs(self, num_epochs):
-        self.num_epochs = num_epochs
-        return
 
-    def train(self, num_epochs=None):
+    def train_epochs(self, num_epochs=None):
         if num_epochs is not None:
             self.num_epochs = num_epochs
         since = time.time()
 
-        # Create a temporary directory to save training checkpoints
-        with TemporaryDirectory() as tempdir:
-            best_model_params_path = os.path.join(tempdir, 'best_model_params.pt')
+        for epoch in range(self.num_epochs):
+            print(f'Epoch {epoch+1}/{self.num_epochs}')
+            print('-' * 10)
 
-            torch.save(self.model.state_dict(), best_model_params_path)
-            best_balanced_acc = 0.0
+            train_loss = self.train()
+            test_loss, metric = self.test()
+            self.scheduler.step()
+            print(f"train loss: {train_loss:.4f}, test loss: {test_loss:.4f}, {self.metric}: {metric:.4f}\n")
+            if self.wandb_log:
+                wandb.log({'train_loss': train_loss, 'valid_loss': test_loss, self.metric: metric})
 
-            for epoch in range(self.num_epochs):
-                print(f'Epoch {epoch}/{self.num_epochs - 1}')
-                print('-' * 10)
-
-                # Each epoch has a training and validation phase
-                for phase in ['train', 'validation']:
-                    if phase == 'train':
-                        self.model.train()  # Set model to training mode
-                    else:
-                        self.model.eval()   # Set model to evaluate mode
-
-                    running_loss = 0.0
-                    running_corrects = 0
-                    balanced_acc_list = []
-
-                    # Iterate over data.
-                    for inputs, labels in self.dataloaders[phase]:
-                        inputs = inputs.to(self.device)
-                        labels = labels.to(self.device)
-
-                        # zero the parameter gradients
-                        self.optimizer.zero_grad()
-
-                        # forward
-                        # track history if only in train
-                        with torch.set_grad_enabled(phase == 'train'):
-                            outputs = self.model(inputs)
-                            _, preds = torch.max(outputs, 1)
-                            loss = self.criterion(outputs, labels)
-
-                            # backward + optimize only if in training phase
-                            if phase == 'train':
-                                loss.backward()
-                                self.optimizer.step()
-
-                        # statistics
-                        running_loss += loss.item() * inputs.size(0)
-                        running_corrects += torch.sum(preds == labels.data)
-                        with warnings.catch_warnings():
-                            warnings.filterwarnings('ignore', category=UserWarning)
-                            balanced_acc_list.append(balanced_accuracy_score(labels.cpu().data, preds.cpu().data))
-                    if phase == 'train':
-                        self.scheduler.step()
-
-                    epoch_loss = running_loss / self.dataset_sizes[phase]
-                    epoch_acc = running_corrects.double() / self.dataset_sizes[phase]
-                    epoch_balanced_acc = np.mean(balanced_acc_list)
-
-                    print(f'{phase} Loss: {epoch_loss:.4f} Acc: {epoch_acc:.4f} balanced_acc: {epoch_balanced_acc:.4f}')
-
-                    # deep copy the model
-                    if phase == 'validation' and epoch_balanced_acc > best_balanced_acc:
-                        best_balanced_acc = epoch_balanced_acc
-                        torch.save(self.model.state_dict(), best_model_params_path)
-
-                    if self.wandb_log and phase == 'validation':
-                        wandb.log({'valid_loss': epoch_loss, 'balanced_accuracy': epoch_balanced_acc})
-
-                print()
-
-            time_elapsed = time.time() - since
-            print(f'Training complete in {time_elapsed // 60:.0f}m {time_elapsed % 60:.0f}s')
-            print(f'Best val balanced_acc: {best_balanced_acc:4f}')
-
-            # load best model weights
-            self.model.load_state_dict(torch.load(best_model_params_path))
-        return self.model, best_balanced_acc
+        time_elapsed = time.time() - since
+        print(f'Training complete in {time_elapsed // 60:.0f}m {time_elapsed % 60:.0f}s')
+        print(f'Final {self.metric}: {metric:4f}\n')
+        return self.model, metric
     
     def unfreeze(self):
         for param in self.model.parameters():
             param.requires_grad = True
         return
+    
+    def train(self):
+        num_batches = len(self.train_dataloader)
+        self.model.train()
+        
+        train_loss = 0
+        for batch, (X, y) in tqdm(enumerate(self.train_dataloader), total=num_batches):
+            X, y = X.to(self.device), y.to(self.device)
+
+            # Compute prediction error
+            pred = self.model(X)
+            loss = self.loss_fn(pred, y)
+
+            # Backpropagation
+            loss.backward()
+            self.optimizer.step()
+            self.optimizer.zero_grad()
+
+            train_loss += loss.item()
+
+        train_loss /= num_batches
+        return train_loss
+
+    def test(self):
+        num_batches = len(self.test_dataloader)
+        self.model.eval()
+        
+        test_loss = 0
+        metric = 0
+        with torch.no_grad():
+            for X, y in self.test_dataloader:
+                X, y = X.to(self.device), y.to(self.device)
+                outputs = self.model(X)
+                _, preds = torch.max(outputs, 1)
+                test_loss += self.loss_fn(outputs, y).item()
+                if self.metric == 'balanced_accuracy':
+                    with warnings.catch_warnings():
+                        warnings.simplefilter('ignore', category=UserWarning)
+                        metric += balanced_accuracy_score(y.cpu().numpy(), preds.cpu().numpy())
+        test_loss /= num_batches
+        metric /= num_batches
+        return test_loss, metric
