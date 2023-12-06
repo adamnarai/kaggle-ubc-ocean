d44e64d731f8260f41a78510ef634e2772fee618
latlab <latlab@gmail.com>
2023-12-07 00:57:22 +0100

    smartcrop_v2

--- src/dataset.py
+++ src/dataset.py
@@ -1,17 +1,18 @@
 import os
 from PIL import Image
 import numpy as np
+import scipy
 
 import torch
 from torch import nn
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms
 from utils import HEDJitter
-    
+
 class UBCTilesDataset(Dataset):
     """UBC OCEAN tiles dataset."""
 
-    def __init__(self, df, image_dir, transform=None, black_th=1.0):
+    def __init__(self, df, image_dir, transform=None):
         """
         Arguments:
             df (DataFrame): DataFrame of annotations.
@@ -26,7 +27,6 @@ class UBCTilesDataset(Dataset):
         self.is_tma = self.df['is_tma'].values
         self.image_dir = image_dir
         self.transform = transform
-        self.black_th = black_th
 
     def __len__(self):
         return len(self.df)
@@ -127,36 +127,38 @@ class SideCrop(nn.Module):
         return new_image
     
 class Affine(nn.Module):
-    def __init__(self, scale):
+    def __init__(self, scale, fill=0):
         super().__init__()
         self.scale = scale
+        self.fill = fill
     def forward(self, image):
-        new_image = transforms.functional.affine(image, angle=0, translate=(0, 0), scale=self.scale, shear=0, fill=255)
+        new_image = transforms.functional.affine(image, angle=0, translate=(0, 0), scale=self.scale, shear=0, fill=self.fill)
         return new_image
     
 class SmartCrop(nn.Module):
+    def split_crop(self, image, dir: str, th: int=0):
+        if dir == 'h':
+            labels, _ = scipy.ndimage.label(image.sum(axis=(0, 2)) > th)
+        elif dir == 'v':
+            labels, _ = scipy.ndimage.label(image.sum(axis=(1, 2)) > th)
+        unique, counts = np.unique(labels, return_counts=True)
+        counts = counts[unique!=0]
+        unique = unique[unique!=0]
+        larges_label = unique[np.argmax(counts)]
+        if dir == 'h':
+            image = image[:, labels==larges_label, :]
+        elif dir == 'v':
+            image = image[labels==larges_label, :, :]
+        return image
+
     def forward(self, image):
         image = np.array(image)
-        # Horizontal crop
-        x_noblack = np.where(image.sum(axis=(0, 2))!=0)[0][0]
-        image = image[:, x_noblack:, :]
-        x_black = np.where(image.sum(axis=(0, 2))==0)[0]
-        if x_black.size > 0 and x_black[0] > 512:
-            x_end = x_black[0]-1
-            image = image[:, :x_end, :]
 
-        # Vertical crop and black trimming
-        y_noblack = np.where(image.sum(axis=(1, 2))!=0)[0][0]
-        image = image[y_noblack:, :, :]
-        y_black = np.where(image.sum(axis=(1, 2))==0)[0]
-        if y_black.size > 0 and y_black[0] > 400:
-            y_end = y_black[0]-1
-            image = image[:y_end, :, :]
-
-        x_black = np.where(image.sum(axis=(0, 2))==0)[0]
-        if x_black.size > 0 and x_black[0] > 400:
-            x_end = x_black[0]-1
-            image = image[:, :x_end, :]
+        # Iterative split and crop
+        image = self.split_crop(image, 'h')
+        image = self.split_crop(image, 'v')
+        image = self.split_crop(image, 'h')
+        image = self.split_crop(image, 'v')
 
         black_bg = np.sum(image, axis=2) == 0
         image[black_bg, :] = 255
@@ -167,6 +169,7 @@ def get_datasets(CFG, train_image_dir, train_thumbnail_dir, df_train, df_validat
     transform = {
     'train':
     transforms.Compose([
+        HEDJitter(theta=CFG['hed_theta']),
         SmartCrop(),
         transforms.RandomAffine(degrees=CFG['affine_degrees'], translate=CFG['affine_translate'], scale=CFG['affine_scale'], fill=255),
         transforms.Resize(CFG['img_size']),
@@ -174,18 +177,17 @@ def get_datasets(CFG, train_image_dir, train_thumbnail_dir, df_train, df_validat
         transforms.RandomHorizontalFlip(),
         transforms.RandomVerticalFlip(),
         # transforms.ColorJitter(**CFG['color_jitter']),
-        # HEDJitter(theta=CFG['hed_theta']),
         transforms.ToTensor(),
-        transforms.Normalize(mean=CFG['img_color_mean'], std=CFG['img_color_std'])
+        # transforms.Normalize(mean=CFG['img_color_mean'], std=CFG['img_color_std'])
     ]),
     'validation':
      transforms.Compose([
         SmartCrop(),
-        Affine(scale=1.2),
+        Affine(scale=np.mean(CFG['affine_scale']), fill=255),
         transforms.Resize(CFG['img_size']),
         transforms.CenterCrop(CFG['img_size']),
         transforms.ToTensor(),
-        transforms.Normalize(mean=CFG['img_color_mean'], std=CFG['img_color_std'])
+        # transforms.Normalize(mean=CFG['img_color_mean'], std=CFG['img_color_std'])
     ])}
     
     train_dataset = UBCDataset(df=df_train, 
@@ -195,6 +197,68 @@ def get_datasets(CFG, train_image_dir, train_thumbnail_dir, df_train, df_validat
     validation_dataset = UBCDataset(df=df_validation, 
                         image_dir=train_image_dir, 
                         thumbnail_dir=train_thumbnail_dir,
+                        transform=transform['validation'])
+    datasets = {'train': train_dataset, 'validation': validation_dataset}
+    return datasets
+
+# Tumor tile detection
+class UBCTumorTilesDataset(Dataset):
+    """UBC OCEAN tiles dataset."""
+
+    def __init__(self, df, image_dir, transform=None):
+        """
+        Arguments:
+            df (DataFrame): DataFrame of annotations.
+            image_dir (string): Directory with all the fullres images.
+            transform (callable, optional): Optional transform to be applied on a sample.
+            black_th (float): Tiles with black > black_th proportion of black pixels are discarded.
+        """
+        self.df = df
+        self.image_ids = self.df['image_id'].values
+        self.orig_image_ids = self.df['orig_image_id'].values
+        self.labels = self.df['label'].values
+        self.image_dir = image_dir
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.df)
+
+    def __getitem__(self, idx):
+        image_id = self.image_ids[idx]
+        orig_image_id = self.orig_image_ids[idx]
+        image_path = os.path.join(self.image_dir, str(orig_image_id), f'{image_id}.png')
+
+        image = Image.open(image_path)
+        label = self.labels[idx]
+        
+        if self.transform:
+            image = self.transform(image)
+
+        return image, label
+
+def get_tumor_tiles_datasets(CFG, train_image_dir, df_train, df_validation):
+    transform = {
+    'train':
+    transforms.Compose([
+        transforms.Resize(CFG['img_size']),
+        transforms.RandomAffine(degrees=CFG['affine_degrees'], translate=CFG['affine_translate'], scale=CFG['affine_scale']),
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomVerticalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=CFG['img_color_mean'], std=CFG['img_color_std'])
+    ]),
+    'validation':
+     transforms.Compose([
+        transforms.Resize(CFG['img_size']),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=CFG['img_color_mean'], std=CFG['img_color_std'])
+    ])}
+    
+    train_dataset = UBCTumorTilesDataset(df=df_train, 
+                        image_dir=train_image_dir,
+                        transform=transform['train'])
+    validation_dataset = UBCTumorTilesDataset(df=df_validation, 
+                        image_dir=train_image_dir,
                         transform=transform['validation'])
     datasets = {'train': train_dataset, 'validation': validation_dataset}
     return datasets
